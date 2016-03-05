0ea1c94918c243fb1689f905e78cf1fd18ead0b8
dimi7ri <vasiliev.dimi@gmail.com>
2016-03-05 22:05:28 +1100

    level3

--- GemStore/app.js
+++ GemStore/app.js
@@ -5,11 +5,34 @@
 		this.products = gems;
 	});
 
+	app.controller("PanelController", function(){
+		this.panel = 1;
+
+		this.selectTab = function(setTab){
+			this.panel = setTab;
+		};
+
+		this.isSelect = function(checkTab){
+			return this.panel === checkTab;
+		};
+	});
+
+	app.controller('GalleryController', function(){
+		this.current = 0;
+ 		this.setCurrent = function(value){
+    	this.current = value || 0;
+  	};
+});
+
 	var gems = [
 	  {
 		name: 'Dodecahedron',
 		price: 2.95,
 		description: 'This is just a description.',
+		specification: 'No specs',
+		reviews: 'Still empty',
+		shine: 3,
+		images:['images/gem-01.gif'],
 		canPurchase: true,
 		soldOut: false,
 	  },
@@ -17,15 +40,23 @@
 		name: 'Pentagonal Gem',
 		price: 5.95,
 		description: ' This is just a description.',
-		canPurchase: false,
+		specification: 'No specs',		
+		reviews: 'Still empty',
+		shine: 4,
+		images:['images/gem-02.gif'],
+		canPurchase: true,
 		soldOut: false,
 	  },
 	  {
-		name: 'Pitagoral Gem',
+	  	name: 'Pitagoral Gem',
 		price: 10.00,
 		description: ' This is just a description.',
+		specification: 'No specs',		
+		reviews: 'Still empty',
+		shine: 7,
+		images:['images/gem-03.gif'],
 		canPurchase: true,
 		soldOut: true,
-	  }	  
+	  }	 
 	];
 })();	
