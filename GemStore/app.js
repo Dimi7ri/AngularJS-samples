d29dc00f262dc383ea39608cf54fedf4219066e3
dimi7ri <vasiliev.dimi@gmail.com>
2016-03-06 20:12:19 +1100

    Level4.

--- GemStore/app.js
+++ GemStore/app.js
@@ -21,8 +21,18 @@
 		this.current = 0;
  		this.setCurrent = function(value){
     	this.current = value || 0;
-  	};
-});
+  		};
+	});
+
+	app.controller('ReviewController', function(){
+		this.review = {};
+
+		this.addReview = function(product){
+			this.review.createdOn = Date.now();
+			product.reviews.push(this.review);
+			this.review = {};	
+		};
+  	});
 
 	var gems = [
 	  {
@@ -30,7 +40,7 @@
 		price: 2.95,
 		description: 'This is just a description.',
 		specification: 'No specs',
-		reviews: 'Still empty',
+		reviews: [{stars:5, body:"5 star review", author:"example@example.com"},{stars:4, body:"4 star review",author:"example2@example2.com"},{stars:3, body:"3 star review",author:"example3@example3.com"}],
 		shine: 3,
 		images:['images/gem-01.gif'],
 		canPurchase: true,
@@ -40,8 +50,8 @@
 		name: 'Pentagonal Gem',
 		price: 5.95,
 		description: ' This is just a description.',
-		specification: 'No specs',		
-		reviews: 'Still empty',
+		specification: 'No specs',
+		reviews: [{stars:5, body:"5 star review", author:"example@example.com"},{stars:4, body:"4 star review",author:"example2@example2.com"},{stars:3, body:"3 star review",author:"example3@example3.com"}],
 		shine: 4,
 		images:['images/gem-02.gif'],
 		canPurchase: true,
@@ -52,7 +62,7 @@
 		price: 10.00,
 		description: ' This is just a description.',
 		specification: 'No specs',		
-		reviews: 'Still empty',
+		reviews: [{stars:5, body:"5 star review", author:"example@example.com"},{stars:4, body:"4 star review",author:"example2@example2.com"},{stars:3, body:"3 star review",author:"example3@example3.com"}],
 		shine: 7,
 		images:['images/gem-03.gif'],
 		canPurchase: true,
